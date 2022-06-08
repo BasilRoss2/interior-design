c1ab5472cc4a5b7de5c0757113f15a9ab07e62ec
Vasiliy Krestyaninov <vasiliy.krestyaninov@ispring.com>
2022-06-08 18:46:59 +0300

    Добавил переключатель фона

--- js/slider.js
+++ js/slider.js
@@ -1,25 +1,44 @@
 const topBackgroundImage = document.querySelector('.b-top');
+const backgroundSwitchers = document.querySelectorAll('.b-top__slder-switcher')
 const sliderSwitcher1 = document.querySelector('#b-top__slder-switcher-1');
 const sliderSwitcher2 = document.querySelector('#b-top__slder-switcher-2');
 const sliderSwitcher3 = document.querySelector('#b-top__slder-switcher-3');
-const activeSwitcher = 'b-top__slder-switcher_active';
+const activeSwitcherClass = 'b-top__slder-switcher_active';
+const backgroundOne = 'center / cover no-repeat url(../images/b-top-background.jpg) scroll';
+const backgroundTwo= 'center / cover no-repeat url(../images/b-top-background-2.jpg) scroll';
+const backgroundThree= 'center / cover no-repeat url(../images/b-top-background-3.jpg) scroll';
+
+function switcherDisable() {
+    backgroundSwitchers.forEach(switcher => {
+        switcher.classList.remove(activeSwitcherClass)
+    })
+}
+
+function changeBackground1() {
+    topBackgroundImage.style.background = backgroundOne;
+    switcherDisable();
+    sliderSwitcher1.classList.add(activeSwitcherClass);
+
+}
+
+function changeBackground2() {
+    topBackgroundImage.style.background = backgroundTwo;
+    switcherDisable();
+    sliderSwitcher2.classList.add(activeSwitcherClass);
+
+}
+
+function changeBackground3() {
+    topBackgroundImage.style.background = backgroundThree;
+    switcherDisable();
+    sliderSwitcher3.classList.add(activeSwitcherClass);
+}
+
 
 sliderSwitcher1.addEventListener('click', changeBackground1);
 
-function changeBackground1() {
-    topBackgroundImage.style.background = 'center / cover no-repeat url(../images/b-top-background.jpg) scroll';
-
-}
-
 sliderSwitcher2.addEventListener('click', changeBackground2);
-
-function changeBackground2() {
-    topBackgroundImage.style.background = 'center / cover no-repeat url(../images/b-top-background-2.jpg) scroll';
-    sliderSwitcher2.classList.add = ('b-top__slder-switcher_active');
-}
 
 sliderSwitcher3.addEventListener('click', changeBackground3);
 
-function changeBackground3() {
-    topBackgroundImage.style.background = 'center / cover no-repeat url(../images/b-top-background-3.jpg) scroll';
-}
+
